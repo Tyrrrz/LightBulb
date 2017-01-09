29c9aefee0402d6331a5b0be1bb7dc7e3cc1bada
Alexey Golub <tyrrrrrr@gmail.com>
2017-01-09 12:42:47 +0200

    Added mutex locking to prevent launching more than one instance of the application

--- LightBulb/App.xaml.cs
+++ LightBulb/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Windows;
+﻿using System.Threading;
+using System.Windows;
 using GalaSoft.MvvmLight.Threading;
 
 namespace LightBulb
 {
     public partial class App
     {
+        private static Mutex _identityMutex;
+
         static App()
         {
             DispatcherHelper.Initialize();
@@ -12,6 +15,17 @@ namespace LightBulb
 
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
+            // Make sure only one process is running at a time
+            // HACK: this waits until the app is fully started before closing it
+            // ... would be better if we could prevent it starting entirely
+            const string mutexName = "LightBulb_Identity";
+            if (Mutex.TryOpenExisting(mutexName, out _identityMutex))
+            {
+                Shutdown();
+                return;
+            }
+            _identityMutex = new Mutex(true, mutexName);
+
             Settings.Default.TryLoad();
         }
 
@@ -19,6 +33,7 @@ namespace LightBulb
         {
             Settings.Default.TrySave();
             Locator.Cleanup();
+            _identityMutex?.ReleaseMutex();
         }
     }
 }
