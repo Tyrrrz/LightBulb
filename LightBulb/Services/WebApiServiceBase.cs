5d02d0490f9541db451e3375df07786d85e118f7
Alexey Golub <tyrrrrrr@gmail.com>
2017-01-13 01:08:55 +0200

    WebApiServiceBase now implements IDIsposable (forgot to add it)

--- LightBulb/Services/WebApiServiceBase.cs
+++ LightBulb/Services/WebApiServiceBase.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace LightBulb.Services
 {
-    public abstract class WebApiServiceBase
+    public abstract class WebApiServiceBase : IDisposable
     {
         private static readonly TimeSpan MinRequestInterval = TimeSpan.FromSeconds(0.2);
         private static DateTime _lastRequestDateTime = DateTime.MinValue;
