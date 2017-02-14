a7beda4b9ccc5b10ef8bd755ea6423ca07656104
Alexey Golub <tyrrrrrr@gmail.com>
2017-02-14 19:58:29 +0200

    Remove environment variable settings

--- LightBulb/Services/Abstract/WebApiServiceBase.cs
+++ LightBulb/Services/Abstract/WebApiServiceBase.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -18,47 +17,8 @@ namespace LightBulb.Services.Abstract
 
         protected WebApiServiceBase()
         {
-            try
-            {
-                WebRequest.DefaultWebProxy = GetEnvironmentProxy();
-            }
-            catch (Exception exc)
-            {
-                Debug.WriteLine("Error during proxy parsing. " + exc.Message);
-            }
-
             _client = new HttpClient();
             _client.DefaultRequestHeaders.Add("User-Agent", "LightBulb (github.com/Tyrrrz/LightBulb)");
-        }
-
-        /// <summary>
-        /// Check for the presence of HTTP_PROXY environment variable.
-        /// The proxy value should follow the pattern : [http://][user:password@]host[:port]
-        /// Example : http://192.168.0.1 or john:doe@theproxy.net:3128
-        /// </summary>
-        /// <returns>IWebProxy if valid proxy found; otherwise null. </returns>
-        private IWebProxy GetEnvironmentProxy()
-        {
-            IWebProxy proxy = null;
-            var envProxy = Environment.GetEnvironmentVariable("HTTP_PROXY");
-            if (envProxy != null)
-            {
-                proxy = new WebProxy(envProxy);
-
-                if (envProxy.StartsWith("http://"))
-                {
-                    envProxy = envProxy.Remove(0, 7);
-                }
-
-                if (envProxy.Contains("@"))
-                {
-                    var tmp = envProxy.Split('@');
-                    var auth = tmp[0].Split(':');
-                    proxy.Credentials = new NetworkCredential(auth[0], auth[1]);
-                }
-            }
-
-            return proxy;
         }
 
         /// <summary>
