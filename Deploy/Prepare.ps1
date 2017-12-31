2cfa20a0e0a3e3ac72e86f06af100e6e3534273d
Alexey Golub <Tyrrrrrr@gmail.com>
2017-12-31 20:22:47 +0200

    Automate chocolatey deployment

--- Deploy/Prepare.ps1
+++ Deploy/Prepare.ps1
@@ -12,3 +12,9 @@ $files += Get-ChildItem -Path "$PSScriptRoot\Portable\*";
 # Pack into archive
 New-Item "$PSScriptRoot\Portable\bin" -ItemType Directory -Force
 $files | Compress-Archive -DestinationPath "$PSScriptRoot\Portable\bin\LightBulb-Portable.zip" -Force
+
+# --- CHOCOLATEY ---
+
+# Create package
+New-Item "$PSScriptRoot\Choco\bin\" -ItemType Directory -Force
+choco pack $PSScriptRoot\Choco\lightbulb.nuspec --out $PSScriptRoot\Choco\bin\
