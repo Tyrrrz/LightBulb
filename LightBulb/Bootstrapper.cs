87807115ce353bfd1ac344f0939369977f0e5ccf
Alexey Golub <Tyrrrrrr@gmail.com>
2019-03-17 20:43:26 +0200

    Mutex

--- LightBulb/Bootstrapper.cs
+++ LightBulb/Bootstrapper.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 using System.Windows.Threading;
 using LightBulb.Services;
 using LightBulb.ViewModels;
@@ -10,6 +12,22 @@ namespace LightBulb
 {
     public class Bootstrapper : Bootstrapper<RootViewModel>
     {
+        // ReSharper disable once NotAccessedField.Local (need to keep reference)
+        private static Mutex _identityMutex;
+
+        public override void Start(string[] args)
+        {
+            // Ensure this is the only running instance, otherwise - exit
+            _identityMutex = new Mutex(true, "LightBulb_Identity", out var isOnlyRunningInstance);
+            if (!isOnlyRunningInstance)
+            {
+                Environment.Exit(0);
+                return;
+            }
+
+            base.Start(args);
+        }
+
         protected override void ConfigureIoC(IStyletIoCBuilder builder)
         {
             base.ConfigureIoC(builder);
