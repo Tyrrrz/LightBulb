22b6438432e21741c714413bf298650b36326637
Alexey Golub <Tyrrrrrr@gmail.com>
2019-04-07 00:55:34 +0300

    Update choco deploy

--- Deploy/Choco/tools/chocolateyinstall.ps1
+++ Deploy/Choco/tools/chocolateyinstall.ps1
@@ -1,15 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
-$packageName= 'lightbulb'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Tyrrrz/LightBulb/releases/download/1.6.4.1/LightBulb-Installer.exe'
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-
-  softwareName  = 'lightbulb*'
-
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  packageName = $env:ChocolateyPackageName
+  softwareName = 'lightbulb*'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  url = 'https://github.com/Tyrrrz/LightBulb/releases/download/1.6.4.1/LightBulb-Installer.exe'
+  fileType = 'exe'
+  silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 Install-ChocolateyPackage @packageArgs
