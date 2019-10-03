9e2482e4a8413caad4ac9a54ff2e7e97278e6477
Alexey Golub <Tyrrrrrr@gmail.com>
2019-10-03 21:03:59 +0300

    Allow manual editing of temperatures

---
+++ LightBulb/Converters/ColorTemperatureToStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using LightBulb.Models;
+
+namespace LightBulb.Converters
+{
+    [ValueConversion(typeof(ColorTemperature), typeof(string))]
+    public class ColorTemperatureToStringConverter : IValueConverter
+    {
+        public static ColorTemperatureToStringConverter Instance { get; } = new ColorTemperatureToStringConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ColorTemperature temperature)
+                return $"{temperature.Value}K";
+
+            return default(string);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string stringValue)
+            {
+                var stringValueCleaned = stringValue.Trim('K').Trim();
+                return new ColorTemperature(double.Parse(stringValueCleaned, CultureInfo.InvariantCulture));
+            }
+
+            return default(ColorTemperature);
+        }
+    }
+}
