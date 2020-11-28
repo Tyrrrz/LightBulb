e15bd6dfd3b657c0b10758b689eb911c120a06f5
Tyrrrz <tyrrrz@gmail.com>
2020-11-29 00:40:59 +0200

    Use JsonExtensions package

--- LightBulb.Domain/GeoLocationProvider.cs
+++ LightBulb.Domain/GeoLocationProvider.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using JsonExtensions.Http;
 using LightBulb.Domain.Internal;
 using LightBulb.Domain.Internal.Extensions;
 
