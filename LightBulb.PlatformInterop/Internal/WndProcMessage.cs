960600d5e9110811acff8e1c92ec63dd86af3808
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-04-07 04:01:42 +0300

    Formatting

--- LightBulb.PlatformInterop/Internal/WndProcMessage.cs
+++ LightBulb.PlatformInterop/Internal/WndProcMessage.cs
@@ -4,7 +4,10 @@ namespace LightBulb.PlatformInterop.Internal;
 
 internal readonly record struct WndProcMessage(uint Id, nint WParam, nint LParam)
 {
-    public T? TryGetLParam<T>() where T : struct =>
+    public T? TryGetLParam<T>()
+        where T : struct =>
         // If LParam is zero, marshaling will fail with null reference exception
-        LParam != 0 ? Marshal.PtrToStructure<T>(LParam) : default(T?);
+        LParam != 0
+            ? Marshal.PtrToStructure<T>(LParam)
+            : default(T?);
 }
