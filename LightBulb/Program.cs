097316df17a9d44202912b0371bdff33d4147552
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-05-24 01:24:47 +0300

    Clean up

--- LightBulb/Program.cs
+++ LightBulb/Program.cs
@@ -17,7 +17,7 @@ public static class Program
 
     public static string VersionString { get; } = Version.ToString(3);
 
-    public static string ExecutableDirPath { get; } = AppDomain.CurrentDomain.BaseDirectory;
+    public static string ExecutableDirPath { get; } = AppContext.BaseDirectory;
 
     public static string ExecutableFilePath { get; } =
         Path.ChangeExtension(Assembly.Location, "exe");
