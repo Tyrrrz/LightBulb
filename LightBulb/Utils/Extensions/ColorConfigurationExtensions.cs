ce528db3273d4204595af58c6cfd1d5ceed7a726
tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2026-04-19 17:57:21 +0300

    asdasd

--- LightBulb/Utils/Extensions/ColorConfigurationExtensions.cs
+++ LightBulb/Utils/Extensions/ColorConfigurationExtensions.cs
@@ -1,35 +1,10 @@
-﻿using System;
+using System;
 using LightBulb.Core;
 
 namespace LightBulb.Utils.Extensions;
 
-internal static class GenericExtensions
+internal static class ColorConfigurationExtensions
 {
-    extension(double value)
-    {
-        public double StepTo(double target, double step) =>
-            target >= value
-                ? Math.Min(value + Math.Abs(step), target)
-                : Math.Max(value - Math.Abs(step), target);
-    }
-
-    extension(DateTimeOffset value)
-    {
-        public DateTimeOffset StepTo(DateTimeOffset target, TimeSpan step)
-        {
-            if (target >= value)
-            {
-                var result = value + step.Duration();
-                return result <= target ? result : target;
-            }
-            else
-            {
-                var result = value - step.Duration();
-                return result >= target ? result : target;
-            }
-        }
-    }
-
     extension(ColorConfiguration value)
     {
         public ColorConfiguration StepTo(
